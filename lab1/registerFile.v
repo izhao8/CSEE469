4385bff2827a10f3d713d560f078f8129dab0e30
Ivan Zhao <qqzaho@gmail.com>
2018-01-13 15:15:56 -0800

    fixed warnings

--- lab1/registerFile.v
+++ lab1/registerFile.v
@@ -10,16 +10,18 @@ module registerFile (data, DRA, DRB, RA, RB, RW, en, clk, reset);
 	input en, clk, reset;
 
 	wire [31:0] wr;
-	wire [63:0] outwr;
+	wire [31:0][63:0] outwr;
 
 	decoder start (.in(RW), .out(wr), .en(en));
 
 	generate
 		genvar i;
-		for (i = 0; i < 32: i = i + 1'b1) begin
-			register set (.in(data), .out(outwr), .en(en), .reset(reset), .clk(clk));
+		for (i = 0; i < 32; i = i + 1'b1) begin
+			register set (.in(data), .out(outwr[i]), .en(wr[i]), .reset(reset), .clk(clk));
 		end
 	endgenerate
+
+	assign outwr[31] = 64'b0;
 
 	multiplex one (.in(outwr), .out(DRA), .read(RA), .clk(clk), .reset(reset));
 	multiplex two (.in(outwr), .out(DRB), .read(RB), .clk(clk), .reset(reset));
