6232681e7da73bbd2230a3440d355f5399dab8ca
Ivan Zhao <qqzaho@gmail.com>
2018-01-13 22:13:36 -0800

    changes

--- lab1/register.v
+++ lab1/register.v
@@ -6,12 +6,12 @@ module register (in, out, en, clk);
 	input [63:0] in;
 	input en, clk;
 
-	in(en) begin
-		generate
-			genvar i;
-			for (i = 0; i < 64; i = i + 1) begin
-				D_FF hold (.q(out[i]), .d(.in[i]), .reset(0), .clk(clk));
+	generate
+		genvar i;
+		if (en) begin 
+			for (i = 0; i < 64; i = i + 1'b1) begin
+				D_FF hold (.q(out[i]), .d(.in[i]), .reset(1'b0), .clk(clk));
 			end
-		endgenerate
-	end
+		end
+	endgenerate
 endmodule
