b40f501a4ed8d82dc710afe8f4819886146d4c5a
Ivan Zhao <qqzaho@gmail.com>
2018-01-14 20:57:39 -0800

    fixed decoder

--- lab1/decoder.v
+++ lab1/decoder.v
@@ -6,7 +6,7 @@ module decoder (in, out, en);
 
 	wire [3:0] dec;
 
-	decoder2to4 first (.in(in[4:3]), .out(dec), .en(1'b1));
+	decoder2to4 first (.in(in[4:3]), .out(dec), .en(en));
 
 	decoder3to8 second (.in(in[2:0]), .out(out[7:0]), .en(dec[0]));
 	decoder3to8 third (.in(in[2:0]), .out(out[15:8]), .en(dec[1]));
@@ -22,12 +22,12 @@ module decoder3to8 (in, out, en);
 	input [2:0] in;
 
 	assign out[0] = ~in[0] & ~in[1] & ~in[2] & en;
-	assign out[1] = ~in[0] & ~in[1] & in[2] & en;
+	assign out[1] = in[0] & ~in[1] & ~in[2] & en;
 	assign out[2] = ~in[0] & in[1] & ~in[2] & en;
-	assign out[3] = ~in[0] & in[1] & in[2] & en;
-	assign out[4] = in[0] & ~in[1] & ~in[2] & en;
+	assign out[3] = in[0] & in[1] & ~in[2] & en;
+	assign out[4] = ~in[0] & ~in[1] & in[2] & en;
 	assign out[5] = in[0] & ~in[1] & in[2] & en;
-	assign out[6] = in[0] & in[1] & ~in[2] & en;
+	assign out[6] = ~in[0] & in[1] & in[2] & en;
 	assign out[7] = in[0] & in[1] & in[2] & en;
 endmodule
 
@@ -39,7 +39,7 @@ module decoder2to4 (in, out, en);
 	input [1:0] in;
 
 	assign out[3] = in[0] & in[1] & en;
-	assign out[2] = in[0] & ~in[1] & en;
-	assign out[1] = ~in[0] & in[1] & en;
+	assign out[2] = ~in[0] & in[1] & en;
+	assign out[1] = in[0] & ~in[1] & en;
 	assign out[0] = ~in[0] & ~in[1] & en;
 endmodule
